a12d200b8b219b3aafd81a3651aefab0c4fae59b
vitorthedev <netd777@gmail.com>
2024-07-31 20:46:27 -0300

    preparar sanitização de nomes de livros e autores

---
+++ fast_zero_projeto_final/sanitize.py
@@ -0,0 +1,23 @@
+import bleach
+
+
+def sanitize_script(text: str):
+    return bleach.clean(text, strip=True)
+
+
+def sanitize_spaces(text: str):
+    no_script: str = sanitize_script(text)
+    split: list[str] = no_script.split(' ')
+    filtered: list[str] = filter(lambda char: char, split)
+    return ' '.join(filtered)
+
+
+def sanitize_case(text: str):
+    return text.lower()
+
+
+def sanitize_nome(nome: str):
+    no_script = sanitize_script(nome)
+    no_space = sanitize_spaces(no_script)
+    no_case = sanitize_case(no_space)
+    return no_case
