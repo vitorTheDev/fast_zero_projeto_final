d1b08a41fd0fc63d5e3d76a1228c02943e89ce5c
vitorthedev <netd777@gmail.com>
2024-07-31 22:16:18 -0300

    testes adicionais de token

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from jwt import decode
 
 from fast_zero_projeto_final.security import create_access_token, settings
@@ -13,3 +15,34 @@ def test_jwt():
 
     assert decoded['test'] == data['test']
     assert decoded['exp']  # Testa se o valor de exp foi adicionado ao token
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/contas/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Credenciais inválidas'}
+
+
+def test_jwt_empty_payload(client):
+    token: str = create_access_token({})
+
+    response = client.delete(
+        '/contas/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Credenciais inválidas'}
+
+
+def test_jwt_user_not_found(client):
+    token: str = create_access_token({'sub': 'test@test.com'})
+
+    response = client.delete(
+        '/contas/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Credenciais inválidas'}
